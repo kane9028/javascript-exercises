915f359790ba8ba759f87f423bcf2630d5263be4
Kane <106299362+kane9028@users.noreply.github.com>
2022-06-28 00:09:46 -0700

    Solve exervise06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // divisible by four but not 100
+    let condition1 = year%4 === 0 && year%100 !== 0;
+    // divisible by 400
+    let condition2 = year%400 === 0;
+    return condition1 || condition2;
 };
 
 // Do not edit below this line
