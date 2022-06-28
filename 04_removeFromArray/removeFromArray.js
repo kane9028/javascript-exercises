b82eaa0760c02199225db40df12bbc9e5e086d2b
Kane <106299362+kane9028@users.noreply.github.com>
2022-06-27 22:57:39 -0700

    Solve exercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    //loop throuch arr. For each element in arr, if it is not found in arguments, push it into the result array
+    let result =[];
+    let argumentsArray = [...arg];
+    arr.forEach(function(el){
+        if (argumentsArray.indexOf(el) === -1) result.push(el);
+    })
+    return result;
 };
 
 // Do not edit below this line
