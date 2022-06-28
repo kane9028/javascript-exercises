43f4a32b391b0282c595c695a639fc5e35f39949
Kane <106299362+kane9028@users.noreply.github.com>
2022-06-27 22:34:07 -0700

    Solve exercise03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //split str into an array
+    let arrOfStr = str.split("");
+    //reverse the array
+    arrOfStr.reverse()
+    //join the reversed array
+    let result = arrOfStr.join("");
+    return result;
 };
-
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
