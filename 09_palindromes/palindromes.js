8f629eaef94d76827be38c526c66e46ebc1f9bdb
Kane <106299362+kane9028@users.noreply.github.com>
2022-07-20 23:38:56 -0700

    Solve exercise09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let strOfLetters = str.replace(/[^\w]/g, '').toLowerCase();
 
+  let reverse = strOfLetters.split('').reduce((arr, letter)=>{
+    arr.unshift(letter);
+    return arr;
+  }, []).join('');
+
+  return strOfLetters === reverse;
 };
 
 // Do not edit below this line
