dd6d3b83682ee83aef44c4f2a2e81749d2bf94ae
Kane <106299362+kane9028@users.noreply.github.com>
2022-07-20 23:23:51 -0700

    Solve exercise08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(!arr[0]) return 0;
+	return arr.reduce((sum,el) => {
+    return sum + el;
+  })
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((pro,el) => {
+    return pro * el;
+  })
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 ) return 1;
+  let result = num;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
