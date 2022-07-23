6a7bd84b626d216b5ee02ad3791fb4903025759e
Kane <106299362+kane9028@users.noreply.github.com>
2022-07-22 21:13:15 -0700

    Solve exercise10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num<0) return "OOPS";
+  let resultArray = [];
+  for (let i = 0; i<num; i++) {
+    if(i===0){
+      resultArray.push(1);  
+    }
+    else if(i===1){
+      resultArray.push(1);  
+    }
+    else {
+      resultArray.push(resultArray[i-1]+resultArray[i-2]);
+    }
+  
+  }
+  return resultArray[num-1];
 };
 
 // Do not edit below this line
