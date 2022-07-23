5a9566b0f44492668b5ebf8d2fc30afbd3bd9a34
Kane <106299362+kane9028@users.noreply.github.com>
2022-07-22 21:51:53 -0700

    Solve exercise12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let date = new Date();
+   return arr.reduce((result, obj) => {
+    let age1 = 0;
+    let age2 = 0;
+    if(result.yearOfDeath){
+      age1 = result.yearOfDeath - result.yearOfBirth;
+    } else {
+      age1 = date.getFullYear() - result.yearOfBirth;
+    }
+    if(obj.yearOfDeath){
+      age2 = obj.yearOfDeath - obj.yearOfBirth;
+    } else {
+      age2 = date.getFullYear() - obj.yearOfBirth;
+    }
+    console.log(result.yearOfDeath, age1);
+    if (age2 > age1) {
+      return obj;
+    } else {
+      return result;
+    }    
+  });
 };
 
 // Do not edit below this line
